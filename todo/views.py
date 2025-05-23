b610f69ef3206eb744a7b5d171c540b8fbd14350
Arash Dev <Arash.alikhani.dev@example.com>
2025-05-23 15:07:28 +0330

    Add Django project files

---
+++ todo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
+
